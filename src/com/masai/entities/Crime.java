7b4255675a4c95cc450ff6ce758b88bd57b94fef
Rupesh0913 <k.rupeshsinha0913@gmail.com>
2023-03-31 00:15:36 +0530

    day-2

--- src/com/masai/entities/Crime.java
+++ src/com/masai/entities/Crime.java
@@ -1,5 +1,41 @@
 package com.masai.entities;
 
 public class Crime {
+	 private String date;
+	    private String crime_id;
+	    private String type;
+	    private String location;
+	    private String description;
+	    
+	    public Crime(String date, String crime_id, String type, String location, String details) {
+	        this.date = date;
+	        this.crime_id = crime_id;
+	        this.type = type;
+	        this.location = location;
+	        this.description = details;
+	    }
+	    
+	    public String getDate() {
+	        return date;
+	    }
+	    
+	    public String getCrime_id() {
+			return crime_id;
+		}
 
+		public String getType() {
+	        return type;
+	    }
+	    
+	    public String getLocation() {
+	        return location;
+	    }
+	    
+	    public String getdescription() {
+	        return description;
+	    }
+	    
+	    public String toString() {
+	        return date + " " + crime_id + " " + type + " " + location + " " + description;
+	    }
 }
